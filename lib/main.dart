72e4ebe44d2582217b68d6c7ab2251b9456cda9d
sitiafiza <iniakunkerja48@gmail.com>
2025-05-04 10:32:01 +0700

    membuat to-do-list

---
+++ lib/main.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class Task {
+  String title;
+  bool isDone;
+
+  Task(this.title, {this.isDone = false});
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do List Mahasiswa',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.pinkAccent,
+        scaffoldBackgroundColor: Color(0xFFE0F7FA), // biru muda
+        fontFamily: 'Roboto',
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.pinkAccent,
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.pinkAccent,
+          foregroundColor: Colors.white,
+        ),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.black87),
+        ),
+      ),
+      home: TodoListPage(),
+    );
+  }
+}
+
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final List<Task> _tasks = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void _addTask(String title) {
+    if (title.trim().isEmpty) return;
+    setState(() {
+      _tasks.add(Task(title));
+      _controller.clear();
+    });
+  }
+
+  void _toggleDone(int index) {
+    setState(() {
+      _tasks[index].isDone = !_tasks[index].isDone;
+    });
+  }
+
+  void _deleteTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+    });
+  }
+
+  void _showAddTaskDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Tambahkan Tugas'),
+        content: TextField(
+          controller: _controller,
+          decoration: InputDecoration(hintText: 'Masukkan nama tugas'),
+          autofocus: true,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              _controller.clear();
+            },
+            child: Text('Batal'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _addTask(_controller.text);
+              Navigator.of(context).pop();
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.pinkAccent,
+              foregroundColor: Colors.white,
+            ),
+            child: Text('Tambah'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('To-Do List Mahasiswa'),
+        centerTitle: true,
+      ),
+      body: _tasks.isEmpty
+          ? Center(
+              child: Text(
+                'Belum ada tugas.\nTambah yuk!',
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+            )
+          : ListView.builder(
+              padding: EdgeInsets.all(12),
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                final task = _tasks[index];
+                return Card(
+                  elevation: 2,
+                  color: Color(0xFFFFEBEE), // pink muda
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  margin: EdgeInsets.symmetric(vertical: 6),
+                  child: ListTile(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    leading: Checkbox(
+                      value: task.isDone,
+                      onChanged: (_) => _toggleDone(index),
+                      activeColor: Colors.pinkAccent,
+                    ),
+                    title: Text(
+                      task.title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        decoration:
+                            task.isDone ? TextDecoration.lineThrough : null,
+                        color: task.isDone ? Colors.grey : Colors.black87,
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete_outline, color: Colors.pink),
+                      onPressed: () => _deleteTask(index),
+                    ),
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddTaskDialog,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
